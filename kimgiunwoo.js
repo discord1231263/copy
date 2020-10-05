a99dfc222bba7e5418a128f265ca120de2190f21
discord1231263 <71957697+discord1231263@users.noreply.github.com>
2020-10-05 10:40:39 +0900

    Update kimgiunwoo.js

--- kimgiunwoo.js
+++ kimgiunwoo.js
@@ -163,6 +163,50 @@ client.on('message', message => {
     }
   }
 });
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('!kick')) {
+    if(checkPermission(message)) return
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`${user.tag} 처리 완료 되었습니다.`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("응 넌 못해~");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("응 넌 못해~");
+    }
+  }
+});
+
+// Log our bot in using the token from https://discord.com/developers/applications
+client.login('your token here');
 function checkPermission(message) {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) {
     message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
