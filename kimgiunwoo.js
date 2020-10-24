3a3d6a6a4d6671decb1eb58f249aa6f76076fdf3
discord1231263 <71957697+discord1231263@users.noreply.github.com>
2020-10-24 15:09:59 +0900

    Update kimgiunwoo.js

--- kimgiunwoo.js
+++ kimgiunwoo.js
@@ -1,135 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.token;
-const welcomeChannelName = "안녕하세요";
-const byeChannelName = "안녕히가세요";
-const welcomeChannelComment = "어서오세요.";
-const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
-  console.log('봇 활성화 완료!');
-});
-
-client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  const newUser = member.user;
-  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
-
-  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
-
-  member.addRole(guild.roles.find(role => role.name == "게스트"));
-});
-
-client.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  const deleteUser = member.user;
-  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
-
-  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
-});
-
-client.on('message', (message) => {
-  if(message.author.bot) return;
-
-  if(message.content == '/zhelp') {
-    return message.reply('```명령어 !전체공지 ```');
-  }
-  if(message.author.bot) return;
-
-  if(message.content == '견우야 안녕') {
-    return message.reply("왜 병신아");
-  }
-  if(message.author.bot) return;
-
-  if(message.content == '나 잘생겼지') {
-    return message.reply("아니 존나 못생겼어 내가 더 잘생긴듯 ");
-  }
-  if(message.author.bot) return;
-
-  if(message.content == '김견우 병신') {
-    return message.reply("그건 인정...");
-  }
-  if(message.content == '정영록 알아?') {
-    return message.reply("아~ 그 멍청이?");
-  }
-  if(message.content == '임민수 알아?') {
-    return message.reply("아~ 그 남고 숙사련ㅋㅋ");
-  }
-  if(message.content == '황우솔 알아?') {
-    return message.reply("아~ 그 메이플 충?");
-  }
-   if(message.content == '이하율 알아?') {
-    return message.reply("아~ 그 임민수 따까리?? https://cdn.discordapp.com/attachments/759667844430168074/759995177422684220/image0.jpg");
-  }
-   if(message.content == '최유민 알아?') {
-    return message.reply("이이잉~~~~ 기뭐링~~~\n https://cdn.discordapp.com/attachments/759706035803521054/760156715756290098/image0.jpg");
-  }
-  if(message.content.startsWith('!전체공지')) {
-    if(!message.member.roles.find(role => role.name === '찬진')) return message.channel.send('너는 챌린저가 아니잖아?!?! 아이언 같은놈이 챌린저 찍고 와라!')
-     .then(msg => msg.delete(3000));  
-    
-     message.reply('pong');
-    if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!전체공지'.length);
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(` ${contents}`);
-      });
+    console.log('봇 활성화 완료!');
+  });
+ 
   
-      return message.reply('공지를 전송했습니다.');
-    } else {
-      return message.reply('채널에서 실행해주세요.');
+client.on('message', (message) => {
+    if(message.author.bot) return;
+  
+    if(message.content == '/짱구명령어') {
+      if(checkPermission(message)) return
+      return message.reply('```명령어 목록 \n /청소 \n !투표 \n```');
     }
-  }
-});
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-  if(checkPermission(message)) return
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('!킥')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`Successfully kicked ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to kick the member');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("That user isn't in this guild!");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("You didn't mention the user to kick!");
-    }
-  }
-});
- if(message.content.startsWith('/cc')) {
+    if(message.content.startsWith('/청소')) {
     if(checkPermission(message)) return
 
-    var clearLine = message.content.slice('/cc '.length);
+    var clearLine = message.content.slice('/청소 '.length);
     var isNum = !isNaN(clearLine)
 
     if(isNum && (clearLine <= 0 || 100 < clearLine)) {
@@ -161,54 +49,10 @@ client.on('message', message => {
         })
         .catch(console.error)
     }
-  };
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return; 
-  if(checkPermission(message)) return
-// If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`${user.tag} 처리 완료 되었습니다.`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to kick the member');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("응 넌 못해~");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("응 넌 못해~");
-    }
   }
+  
 });
 
-// Log our bot in using the token from https://discord.com/developers/applications
-client.login('your token here');
 function checkPermission(message) {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) {
     message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
@@ -217,6 +61,7 @@ function checkPermission(message) {
     return false;
   }
 }
+
 function changeCommandStringLength(str, limitLen = 8) {
   let tmp = str;
   limitLen -= tmp.length;
@@ -228,7 +73,7 @@ function changeCommandStringLength(str, limitLen = 8) {
   return tmp;
 }
 
-async function AutoMsgDelete(message, str, delay = 3000) {
+async function AutoMsgDelete(message, str, delay = 1000) {
   let msg = await message.channel.send(str);
 
   setTimeout(() => {
@@ -236,4 +81,6 @@ async function AutoMsgDelete(message, str, delay = 3000) {
   }, delay);
 }
 
+  
+  
 client.login(token);
